c774f5160578f23a793d141860f1eef4b10faf96
Srikanth Kyatham <srikanthkyatham@Srikanths-MacBook-Pro.local>
2022-10-15 21:32:25 -0500

    Added Task Four

--- src/main/scala/TaskFour/TaskFourMapReduce.scala
+++ src/main/scala/TaskFour/TaskFourMapReduce.scala
@@ -1,35 +1,31 @@
 package com.srikanth.cs441
-package TaskThree
+package TaskFour
 
-import com.srikanth.cs441.CommonUtil.GetConfigRef
-import com.srikanth.cs441.CommonUtil.GetConfigRef.{checkRegexPattern, getLogMessageTypes}
-import com.srikanth.cs441.TaskOne.TaskOneMapReduce.{TaskOneMapper, TaskOneReducer}
+import CommonUtil.GetConfigRef.{checkRegexPattern, getLogMessageTypes}
+import scala.collection.JavaConverters.*
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
 import org.apache.hadoop.mapred.{FileInputFormat, FileOutputFormat, JobClient, JobConf, MapReduceBase, Mapper, OutputCollector, Reducer, Reporter, TextInputFormat, TextOutputFormat}
-
-import scala.collection.JavaConverters.*
-import java.util
 import org.slf4j.LoggerFactory
 
 import java.io.IOException
-object TaskThreeMapReduce :
-  class TaskThreeMapper extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable] :
-    private final val one = new IntWritable(1)
+import java.util
+
+object TaskFourMapReduce :
+  class TaskFourMapper extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable] :
     private val logger = LoggerFactory.getLogger(getClass)
 
     @throws[IOException]
     override def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
       val logMessageTypes = getLogMessageTypes.r
-      val line = value.toString
-
-      if (checkRegexPattern(line, logMessageTypes)) {
-        output.collect(new Text(logMessageTypes.findFirstIn(line).get), one)
+      val messageList: Array[String] = value.toString.split(" - ").map(str => str.trim())
+      if (checkRegexPattern(value.toString, logMessageTypes)) {
+        output.collect(new Text(logMessageTypes.findFirstIn(value.toString).get),IntWritable(messageList(messageList.length-1).toCharArray.length))
       }
 
-  class TaskThreeReducer extends MapReduceBase with Reducer[Text, IntWritable, Text, IntWritable] :
+  class TaskFourReducer extends MapReduceBase with Reducer[Text, IntWritable, Text, IntWritable] :
     override def reduce(key: Text, values: util.Iterator[IntWritable], output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
-      val sum = values.asScala.reduce((valueOne, valueTwo) => new IntWritable(valueOne.get() + valueTwo.get()))
+      val sum = values.asScala.reduce((valueOne, valueTwo) => new IntWritable(valueOne.get().max(valueTwo.get())))
       output.collect(key, new IntWritable(sum.get()))
 
 
@@ -43,9 +39,9 @@ object TaskThreeMapReduce :
     conf.set("mapred.textoutputformat.separator", ",");
     conf.setOutputKeyClass(classOf[Text])
     conf.setOutputValueClass(classOf[IntWritable])
-    conf.setMapperClass(classOf[TaskThreeMapper])
-    conf.setCombinerClass(classOf[TaskThreeReducer])
-    conf.setReducerClass(classOf[TaskThreeReducer])
+    conf.setMapperClass(classOf[TaskFourMapper])
+    conf.setCombinerClass(classOf[TaskFourReducer])
+    conf.setReducerClass(classOf[TaskFourReducer])
     conf.setInputFormat(classOf[TextInputFormat])
     conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
     FileInputFormat.setInputPaths(conf, new Path(inputPath))
